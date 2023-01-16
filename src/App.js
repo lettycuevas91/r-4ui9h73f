cc95e0c9fd4ca52b0042ef3dbd58a4f64597953b
Leticia <lettycuevas91@gmail.com>
2023-01-16 20:51:23 -0300

    test

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.count = this.count.bind(this);
+    this.state = { count: 0 };
+  }
+
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text"  onChange={this.count} placeholder="Empieza a escribir algo" />
+        <p className="repeater">{this.state.count}</p>
       </div>
     );
+  }
+  count(e) {
+    this.setState({
+      count: e.target.value
+    });
   }
 }
 
